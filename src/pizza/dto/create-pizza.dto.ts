62384ba61f51851aaa448988a4cfff614bc3429a
ali <ali.f90@outlook.com>
2022-05-04 15:30:42 +0200

    Installed Dep for Validation DTO && Initial Data Modeling

--- src/pizza/dto/create-pizza.dto.ts
+++ src/pizza/dto/create-pizza.dto.ts
@@ -1,1 +1,26 @@
-export class CreatePizzaDto {}
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+
+export enum PizzaSize {
+  small = 'SMALL',
+  medium = 'MEDIUM',
+  large = 'LARGE',
+}
+
+export class CreatePizzaDto {
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  size: PizzaSize;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @MaxLength(20, {
+    each: true,
+  })
+  ingredients: string[];
+}
