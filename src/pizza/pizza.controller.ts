d1e5c71b1f8f1060beeea4c8a5a20bfad4dddb86
ali <ali.f90@outlook.com>
2022-05-06 10:11:57 +0200

    Updated The API to work with Database && Updated The Postman Collection API File

--- src/pizza/pizza.controller.ts
+++ src/pizza/pizza.controller.ts
@@ -10,37 +10,37 @@ import {
 import { PizzaService } from './pizza.service';
 import { CreatePizzaDto } from './dto/create-pizza.dto';
 import { UpdatePizzaDto } from './dto/update-pizza.dto';
-import { Pizza } from './entities/pizza.entity';
+import { IPizza } from '../schemas/pizza.schema';
 
 @Controller('pizza')
 export class PizzaController {
   constructor(private readonly pizzaService: PizzaService) {}
 
   @Post()
-  create(@Body() createPizzaDto: CreatePizzaDto) {
+  create(@Body() createPizzaDto: CreatePizzaDto): Promise<IPizza> {
     return this.pizzaService.create(createPizzaDto);
   }
 
-  // @Get()
-  // findAll(): Pizza[] {
-  //   return this.pizzaService.findAll();
-  // }
+  @Get()
+  findAll(): Promise<IPizza[]> {
+    return this.pizzaService.findAll();
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string): Pizza {
-  //   return this.pizzaService.findOne(id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<IPizza> {
+    return this.pizzaService.findOne(id);
+  }
 
-  // @Patch(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updatePizzaDto: UpdatePizzaDto,
-  // ): Pizza {
-  //   return this.pizzaService.update(id, updatePizzaDto);
-  // }
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updatePizzaDto: UpdatePizzaDto,
+  ): Promise<IPizza> {
+    return this.pizzaService.update(id, updatePizzaDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string): string {
-  //   return this.pizzaService.remove(id);
-  // }
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<string> {
+    return this.pizzaService.remove(id);
+  }
 }
--- src/pizza/pizza.service.ts
+++ src/pizza/pizza.service.ts
@@ -17,34 +17,53 @@ export class PizzaService {
       const newPizza = await this.pizzaModel.create(createPizzaDto);
       return newPizza;
     } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async findAll(): Promise<IPizza[]> {
+    try {
+      const pizzas = await this.pizzaModel.find();
+      return pizzas;
+    } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  // findAll(): Pizza[] {
-  //   return this.pizzas;
-  // }
+  async findOne(id: string): Promise<IPizza> {
+    try {
+      const pizza = await this.pizzaModel.findById(id);
+      if (!pizza)
+        throw new HttpException('Pizza is Not Found', HttpStatus.NOT_FOUND);
+      return pizza;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-  // findOne(id: string): Pizza {
-  //   const pizzaToReturn = this.pizzas.find((pizza) => pizza.id === id);
-  //   if (!pizzaToReturn)
-  //     throw new HttpException("Pizza's Is not Found", HttpStatus.NOT_FOUND);
-  //   return pizzaToReturn;
-  // }
+  async update(id: string, updatePizzaDto: UpdatePizzaDto): Promise<IPizza> {
+    try {
+      const pizzaToUpdate = await this.pizzaModel.findByIdAndUpdate(
+        id,
+        updatePizzaDto,
+        { new: true },
+      );
+      if (!pizzaToUpdate)
+        throw new HttpException('Pizza is Not Found', HttpStatus.NOT_FOUND);
+      return pizzaToUpdate;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-  // update(id: string, updatePizzaDto: UpdatePizzaDto): Pizza {
-  //   let pizzaToUpdate = this.pizzas.find((pizza) => pizza.id === id);
-  //   if (!pizzaToUpdate)
-  //     throw new HttpException("Pizza's Is not Found", HttpStatus.NOT_FOUND);
-  //   pizzaToUpdate = { ...pizzaToUpdate, ...updatePizzaDto };
-  //   return pizzaToUpdate;
-  // }
-
-  // remove(id: string): string {
-  //   const pizzaToDelete = this.pizzas.find((pizza) => pizza.id === id);
-  //   if (!pizzaToDelete)
-  //     throw new HttpException("Pizza's Is not Found", HttpStatus.NOT_FOUND);
-  //   this.pizzas = this.pizzas.filter((pizza) => pizza.id !== id);
-  //   return `Pizza with Id: ${id} have been Deleted`;
-  // }
+  async remove(id: string): Promise<string> {
+    try {
+      const pizzaToDelete = await this.pizzaModel.findByIdAndDelete(id);
+      if (!pizzaToDelete)
+        throw new HttpException('Pizza is Not Found', HttpStatus.NOT_FOUND);
+      return 'Pizza is Deleted Successfully';
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
