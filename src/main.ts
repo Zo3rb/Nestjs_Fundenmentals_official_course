d594e74585dc5590287fc742d0904006cbaf100c
ali <ali.f90@outlook.com>
2022-05-07 13:20:15 +0200

    Added Swagger for open API and API Documentation.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Initiating the Application.
   const app = await NestFactory.create(AppModule);
+
+  // Using Global Validation.
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -14,6 +19,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  // Using Swagger.
+  const config = new DocumentBuilder()
+    .setTitle('I Love Pizza API Documentation.')
+    .setDescription('an API to Create Your Own Pizza as You love.')
+    .setVersion('1.0')
+    .addTag('pizza')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
